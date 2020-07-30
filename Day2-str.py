ad22fcd6d8a2c14a6e777ddc19b4c40b6e98fc01
jasmehra <jasmehra13197@gmail.com>
2020-07-30 23:56:32 +0530

    ommit

---
+++ Day2-str.py
@@ -0,0 +1,16 @@
+#data type "str"
+
+print(type("My name is jasleen!"))
+frst_name = 'Jasleen'
+last_name = 'Kaur'
+full_name = frst_name +' '+ last_name
+print(full_name) 
+
+#for multi ie string we will use ''' triple quotes
+
+long_str = '''Hey
+WOW
+o o
+---
+'''
+print(long_str)
