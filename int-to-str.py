bf360a44e60b2d63da87b9d2c4e5be254349dc7c
jasmehra <jasmehra13197@gmail.com>
2020-07-30 23:57:38 +0530

    commit

---
+++ int-to-str.py
@@ -0,0 +1,14 @@
+#string concatenation
+
+print("Jasleen" + " Kaur")
+#print("Jasleen" + 4)   #gives error
+
+#so we can only concatenate strings with this operator
+
+print("Jasleen" + str(5))
+
+print(type(str(4)))
+print(type(int(str(500))))  
+
+#so it is line type conversion we are converting str into int and thus intp many more like float.
+
