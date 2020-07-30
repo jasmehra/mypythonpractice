9bcfe7ec3302a27e5ba9ba7839e3d3b537ed9fed
jasmehra <jasmehra13197@gmail.com>
2020-07-30 09:15:34 +0530

    My commit in testing branch

---
+++ Day2-data-types.py
@@ -0,0 +1,17 @@
+print(7+6)
+print(type(6))
+print(type(9.1 + 0.8))
+
+print(2 ** 3) #power off
+
+print(5 // 4) #dounble multiplication
+
+print(5 % 4) # modulo
+
+
+#math functions
+
+print(round(3.4))
+print(round(3.6))
+print(abs(-20))
+
